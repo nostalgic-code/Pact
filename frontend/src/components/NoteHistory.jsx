897209938d02097b9893a4d0f68d118bdcdd75fd
nostalgic-code <151646857+nostalgic-code@users.noreply.github.com>
2025-05-13 10:52:42 +0200

    Update NoteHistory.jsx
    link update

--- frontend/src/components/NoteHistory.jsx
+++ frontend/src/components/NoteHistory.jsx
@@ -24,7 +24,7 @@ export default function NoteHistory({ pactId, isOpen, onClose }) {
         if (!user) return
 
         const token = await user.getIdToken()
-        const response = await fetch(`http://127.0.0.1:8000/api/pacts/${pactId}/checkins/`, {
+        const response = await fetch(`https://3453-102-39-173-168.ngrok-free.app/api/pacts/${pactId}/checkins/`, {
           headers: { Authorization: token }
         })
 
