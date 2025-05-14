5e6fc6f80a49759898ee801c10649a200ac93759
nostalgic-code <151646857+nostalgic-code@users.noreply.github.com>
2025-05-14 09:40:48 +0200

    Update checkIn.jsx

--- frontend/src/components/checkIn.jsx
+++ frontend/src/components/checkIn.jsx
@@ -31,7 +31,7 @@ export default function TodaysCheckInCard({ pactId, onCheckIn }) {
 
       const idToken = await currentUser.getIdToken()
 
-      const response = await fetch("https://e665-105-214-49-222.ngrok-free.app/api/checkins/", {
+      const response = await fetch("https://9de1-102-39-173-168.ngrok-free.app/api/checkins/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
