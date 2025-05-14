d47b2875ee1188890f69b147219c7d2e8745eb03
nostalgic-code <151646857+nostalgic-code@users.noreply.github.com>
2025-05-14 12:45:40 +0200

    Update NotesPage.jsx

--- frontend/src/components/NotesPage.jsx
+++ frontend/src/components/NotesPage.jsx
@@ -20,7 +20,7 @@ export default function NotesPage() {
         if (!user) return
 
         const token = await user.getIdToken()
-        const response = await fetch("https://9de1-102-39-173-168.ngrok-free.app/api/checkins/", {
+        const response = await fetch("https://1756-102-39-173-168.ngrok-free.app/api/checkins/", {
           headers: { Authorization: token }
         })
 
