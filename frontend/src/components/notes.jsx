4b7641b65394302d6afc569c6e5b5f159fdc2fdf
nostalgic-code <151646857+nostalgic-code@users.noreply.github.com>
2025-05-14 12:49:16 +0200

    Update notes.jsx

--- frontend/src/components/notes.jsx
+++ frontend/src/components/notes.jsx
@@ -23,7 +23,7 @@ export default function NotesCard() {
 
       const token = await user.getIdToken()
 
-      const res = await fetch("https://9de1-102-39-173-168.ngrok-free.app/api/checkins/note", {
+      const res = await fetch("https://1756-102-39-173-168.ngrok-free.app/api/checkins/note", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
