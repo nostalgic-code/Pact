6c0a1803c4a61e84bf0af6979846d05faf706bfd
nostalgic-code <151646857+nostalgic-code@users.noreply.github.com>
2025-05-19 13:46:39 +0200

    Update todaysOverview.jsx

--- frontend/src/components/todaysOverview.jsx
+++ frontend/src/components/todaysOverview.jsx
@@ -91,7 +91,7 @@ export default function TodaysOverview() {
 
       try {
         const token = await user.getIdToken()
-        const res = await fetch("https://56ff-102-39-173-168.ngrok-free.app/api/pacts", {
+        const res = await fetch("https://54e5-102-39-173-168.ngrok-free.app/api/pacts", {
           headers: { Authorization: token },
         })
 
