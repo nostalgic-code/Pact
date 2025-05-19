63a9c1a06589763ffbf5e4d6b9462462f4ebeba0
nostalgic-code <151646857+nostalgic-code@users.noreply.github.com>
2025-05-19 13:44:12 +0200

    Update createPactCard.jsx

--- frontend/src/components/createPactCard.jsx
+++ frontend/src/components/createPactCard.jsx
@@ -38,7 +38,7 @@ export default function CreatePactCard() {
       }
 
       const idToken = await currentUser.getIdToken()
-      const response = await fetch("https://56ff-102-39-173-168.ngrok-free.app/api/pacts/", {
+      const response = await fetch("https://54e5-102-39-173-168.ngrok-free.app/api/pacts/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
